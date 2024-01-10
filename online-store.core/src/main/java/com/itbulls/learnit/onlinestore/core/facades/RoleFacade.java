db6b9c1732ea9b5e5fbfacd026a0c66c324f149b
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2024-01-10 22:45:05 +0800

    Delete online-store.core directory

--- online-store.core/src/main/java/com/itbulls/learnit/onlinestore/core/facades/RoleFacade.java
+++
@@ -1,9 +0,0 @@
-package com.itbulls.learnit.onlinestore.core.facades;
-
-import com.itbulls.learnit.onlinestore.persistence.entities.Role;
-
-public interface RoleFacade {
-
-	Role getRoleByName(String roleName);
-
-}
